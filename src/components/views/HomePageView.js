e8cdce37d5412ca009b80e2a215d1628a15c1855
alksdjesus <56933580+alksdjesus@users.noreply.github.com>
2022-12-08 21:59:14 -0500

    small css additions

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,16 +1,23 @@
-
-
 import { Link } from 'react-router-dom';
-
 
 
 const HomePageView = () => {
   return (
-    <div>
-      <h6>Final Project</h6>
-      <Link to={'/employees'} > All Employees </Link>
-      <Link to={'/tasks'} > All Tasks </Link>
-      
+    <div className='container'>
+      <ul>
+      <div className='header'>
+        Assignment 3
+      </div>
+      <br/><br/><br/><br/>
+      <div className='links_container'>
+        <div className='links'>
+          <Link to={'/employees'} > All Employees </Link>
+        </div>
+        <div className='links'>
+          <Link to={'/tasks'} > All Tasks </Link>
+        </div>
+      </div>
+      </ul>
     </div>
   );    
 }
