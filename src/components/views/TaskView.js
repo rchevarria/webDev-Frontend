ee2fc9988d798cf22ace4d98b010374410d87206
Mingmar Dolma <62723387+mingmardolma@users.noreply.github.com>
2022-12-18 13:22:17 -0500

    updated taskView

--- src/components/views/TaskView.js
+++ src/components/views/TaskView.js
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom";
+import { BackButton, NormButton, SmallButton } from "../buttons";
 
 const TaskView = (props) => {
   const { task } = props;
   return (
-    <div className='container'>
+    <div >
       <Link to={`/tasks`}>
          <button>Back</button>
       </Link>
-      <h1>{task.description}</h1>
-      {task.employee ? <h3>{task.employee.firstname + " " + task.employee.lastname}</h3>: <h3> unassigned </h3>}
+      <h1>Task: {task.description}</h1>
+      <div>
+        Assigned to: {task.employee ? <h3>{task.employee.firstname + " " + task.employee.lastname}</h3>: <h3> unassigned </h3>}
+
+      </div>
+      {/* Assigned to: {task.employee ? <h3>{task.employee.firstname + " " + task.employee.lastname}</h3>: <h3> unassigned </h3>} */}
     
-      <Link to={`/edittask/${task.id}`}>Edit task information</Link>
+      <NormButton to={`/edittask/${task.id}`}>
+        Edit Task Information
+      </NormButton>
       <br/>
-      <Link to={`/tasks`}>View all tasks</Link>
+      {/* <br/> */}
+
+      {/* <Link to={`/edittask/${task.id}`}>Edit task information</Link> */}
+      <br/>
+      {/* <Link to={`/tasks`}>View all tasks</Link> */}
+      <NormButton to={`/tasks`}>
+        View All Tasks
+      </NormButton>
     </div>
   );
 
